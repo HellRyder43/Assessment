8ea7002fa77add25cca02b8647e3ba2b5c47983f
Amir Hamzah <46454997+HellRyder43@users.noreply.github.com>
2023-03-29 22:42:55 +0800

    Added models for Employee and TaxInformation. Stored tax rate information in json file.

---
+++ Smartly/Model/TaxInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smartly.Model
+{
+    public class TaxInfo
+    {
+        public int LowerLimit { get; set; }
+        public int UpperLimit { get; set; }
+        public double TaxRate { get; set; }
+    }
+
+}
